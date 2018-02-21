30cbc7c5c7529de2c9666ff7fdfcbd8d81f01576
suresh <36321752+sureshlohith@users.noreply.github.com>
2018-02-21 23:30:49 +0530

    changed for jenkins tutorial

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		for(int i=0;i<=10;i++) {
-			System.out.println("WELCOME TO DEVOPS  "+i);
+			System.out.println("WELCOME TO JENKINS  "+i);
 		}
 
 	}
